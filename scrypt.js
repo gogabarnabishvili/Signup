bc341b3832de80b3bb85ae9aa988386765936dc1
gogabarnabishvili <99512619+gogabarnabishvili@users.noreply.github.com>
2023-01-15 02:38:39 +0400

    error js

--- scrypt.js
+++ scrypt.js
@@ -20,18 +20,51 @@ function darkMode() {
     }
 }
 
+function isValidEmail(email) {
+    let re = /^(([^<>()[].,;:\s@"]+(.[^<>()[].,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,})).ge$/;
+    return re.test(String(email).toLowerCase());
+}
+
 const emailErr = document.querySelector(".errorTextEmail")
 fomrInput[1].addEventListener("click", () => {
     fomrInput[1].value = ""
 })
-console.log(emailErr)
-fomrInput[4].addEventListener("click", () => {
-        if (fomrInput[0] === "") {
-            emailErr.innerHTML = "please chack email !";
-        }
-    })
-    // const form = document.querySelectorAll("form");
-    // console.log(fomrInput[4])
-    // fomrInput[4].addEventListener("keyup", () => {
-    //     if (fomrInput[0] === "")
-    // })
+
+fomrInput[4].addEventListener("click", (e) => {
+    e.preventDefault
+
+    if (fomrInput[0].value === "") {
+        emailErr.innerHTML = "Please enter an Email !";
+        fomrInput[0].style.border = "1px solid red";
+    } else if (!isValidEmail(fomrInput[0])) {
+        emailErr.innerHTML = "The email does not end with ge !";
+        fomrInput[0].style.border = "1px solid red";
+    } else {
+        fomrInput[0].style.border = "1px solid green"
+        emailErr.innerHTML = "";
+    }
+    const errorTextCountry = document.querySelector(".errorTextCountry");
+    if (fomrInput[1].value === "") {
+        errorTextCountry.innerHTML = "Please enter an Country !";
+        fomrInput[1].style.border = "1px solid red";
+    } else {
+        fomrInput[1].style.border = "1px solid green"
+        errorTextCountry.innerHTML = "";
+    }
+    const errorTextCity = document.querySelector(".errorTextCity");
+    if (fomrInput[2].value === "") {
+        errorTextCity.innerHTML = "Please enter an City !";
+        fomrInput[2].style.border = "1px solid red";
+    } else {
+        fomrInput[2].style.border = "1px solid green"
+        errorTextCity.innerHTML = "";
+    }
+    const errorTextZip = document.querySelector(".errorTextZip");
+    if (fomrInput[3].value === "") {
+        errorTextZip.innerHTML = "Please enter an Zip Code !";
+        fomrInput[3].style.border = "1px solid red";
+    } else {
+        fomrInput[3].style.border = "1px solid green"
+        errorTextZip.innerHTML = "";
+    }
+})
